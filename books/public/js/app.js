254cbc8e5fde0c34fcfdd08beb20efe0955b5d3e
EC2 Default User <ec2-user@ip-172-31-37-8.us-east-2.compute.internal>
2021-04-02 09:15:50 +0000

    会員登録機能の実装

--- books/public/js/app.js
+++ books/public/js/app.js
@@ -49993,7 +49993,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nValidationError: Invalid options object. Sass Loader has been initialized using an options object that does not match the API schema.\n - options has an unknown property 'outputStyle'. These properties are valid:\n   object { implementation?, sassOptions?, prependData?, sourceMap?, webpackImporter? }\n    at validate (/home/ec2-user/environment/books/node_modules/schema-utils/dist/validate.js:98:11)\n    at Object.loader (/home/ec2-user/environment/books/node_modules/sass-loader/dist/index.js:36:28)\n    at runLoaders (/home/ec2-user/environment/books/node_modules/webpack/lib/NormalModule.js:316:20)\n    at /home/ec2-user/environment/books/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /home/ec2-user/environment/books/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at runSyncOrAsync (/home/ec2-user/environment/books/node_modules/loader-runner/lib/LoaderRunner.js:143:3)\n    at iterateNormalLoaders (/home/ec2-user/environment/books/node_modules/loader-runner/lib/LoaderRunner.js:232:2)\n    at /home/ec2-user/environment/books/node_modules/loader-runner/lib/LoaderRunner.js:205:4\n    at process.nextTick (/home/ec2-user/environment/books/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:85:15)\n    at process._tickCallback (internal/process/next_tick.js:61:11)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 
