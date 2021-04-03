7be9e1226568f1ee7475b73d759b4f239ec8ec55
mzw86020424 <mzw86020424@gmail.com>
2021-04-03 06:11:01 +0000

    レビュー登録機能を実装

--- books/resources/views/review.blade.php
+++ books/resources/views/review.blade.php
@@ -1,25 +1,31 @@
 @extends('layouts.app')
 
+@extends('layouts.app')
+
 @section('content')
-<h1	class="pagetitle">レビュー投稿ページ</h1>
-<div class="rouw justify-content-center container">
-	<div class="coll-md-10">
-		<form method='POST' action="" enctype="multipart/form-data">
-			@csrf
-			<div class="card">
-				<div class="card-body">
-					<div class="form-group">
-						<label>レビュー本文</label>
-						<textarea class="description form-control" name='body' placeholder="本文を入力"></textarea>
-					</div>
-					<div class="form-group">
-						<label for="file 1">本のサムネイル </label>
-						<input type="file" id="file 1" name="image" class="form-control-file">
-					</div>
-					<input type='submit' class='btn btn-primary' value='レビューを登録'>
-				</div>
-			</div>
-		</form>
-	</div>
+<h1 class='pagetitle'>レビュー投稿ページ</h1>
+<div class="row justify-content-center container">
+    <div class="col-md-10">
+      <form method='POST' action="{{ route('store') }}" enctype="multipart/form-data">
+        @csrf
+        <div class="card">
+            <div class="card-body">
+              <div class="form-group">
+                <label>本のタイトル</label>
+                <input type='text' class='form-control' name='title' placeholder='タイトルを入力'>
+              </div>
+              <div class="form-group">
+              <label>レビュー本文</label>
+                <textarea class='description form-control' name='body' placeholder='本文を入力'></textarea>
+              </div>
+              <div class="form-group">
+                <label for="file1">本のサムネイル</label>
+                <input type="file" id="file1" name='image' class="form-control-file">
+              </div>
+              <input type='submit' class='btn btn-primary' value='レビューを登録'>
+            </div>
+        </div>
+      </form>
+    </div>
 </div>
 @endsection
