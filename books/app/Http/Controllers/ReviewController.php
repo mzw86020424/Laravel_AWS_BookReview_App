bbaa51827e86364376b0fda3c13ac3076264223d
mzw86020424 <mzw86020424@gmail.com>
2021-04-03 07:11:47 +0000

    レビュー投稿のバリデーションを実装

--- books/app/Http/Controllers/ReviewController.php
+++ books/app/Http/Controllers/ReviewController.php
@@ -22,6 +22,12 @@ class ReviewController extends Controller
     {
         $post = $request->all();
         
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+            'image' => 'mimes:jpeg,png,jpg,gif,svgImax:2048',
+            ]);
+        
         if($request->hasFile('image')) {
         $request->file('image')->store('/public/images');
         $data = ['user_id' => \Auth::id(), 'title' => $post['title'], 'body' => $post['body'], 'image' => $request->file('image')->hashName()];
